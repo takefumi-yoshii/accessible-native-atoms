ae3cc16c7d3d726211c3b0d97cd82bc4688e2937
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-06-22 21:04:50 +0900

    faet(component): add Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,70 @@
 import React from "react";
 import styles from "./style.module.scss";
 import { mergeClassName } from "../utils";
+// ______________________________________________________
+//
+type Color = "primary" | "secondary";
+type SpecificProp = { color?: Color };
+type Tag = "button" | "a" | "input";
+// ______________________________________________________
+//
+type TagProp = { tag: Tag }; // タグ特定
+// ______________________________________________________
+//
+type InnerProps<T extends TagProp> = SpecificProp &
+  React.ComponentPropsWithoutRef<T["tag"]>;
+type Props<T extends TagProp> = InnerProps<T> & T;
+// ______________________________________________________
+//
+const ButtonComponent = React.forwardRef<
+  HTMLButtonElement,
+  InnerProps<{ tag: "button" }>
+>(({ className, color = "primary", ...props }, ref) => (
+  <button
+    ref={ref}
+    tabIndex={-1}
+    className={mergeClassName(styles.button, className)}
+    data-color={color}
+    {...props}
+  />
+));
 
-type Props = React.ComponentProps<"button"> & {
-  theme: "primary" | "secondary";
-};
+const InputComponent = React.forwardRef<
+  HTMLInputElement,
+  InnerProps<{ tag: "input" }>
+>(({ className, color = "primary", ...props }, ref) => (
+  <input
+    ref={ref}
+    tabIndex={-1}
+    className={mergeClassName(styles.button, className)}
+    data-color={color}
+    type="button"
+    {...props}
+  />
+));
 
-export const Button = React.forwardRef<HTMLButtonElement, Props>(
-  ({ theme, className, ...props }, ref) => (
-    <button
-      ref={ref}
-      data-theme={theme}
-      className={mergeClassName(styles.button, className)}
-      {...props}
-    />
-  )
-);
+const AnchorComponent = React.forwardRef<
+  HTMLAnchorElement,
+  InnerProps<{ tag: "a" }>
+>(({ className, color = "primary", ...props }, ref) => (
+  <a
+    ref={ref}
+    tabIndex={-1}
+    className={mergeClassName(styles.button, className)}
+    data-color={color}
+    {...props}
+    role="button" // role は強制される
+  />
+));
+// ______________________________________________________
+//
+export function Button<T extends TagProp>({ tag, ...props }: Props<T>) {
+  switch (tag) {
+    case "a":
+      return <AnchorComponent {...props} />;
+    case "button":
+      return <ButtonComponent {...props} />;
+    default:
+      return <InputComponent {...props} />;
+  }
+}
