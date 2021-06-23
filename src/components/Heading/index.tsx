1c407bc49be24311e1d51711c15867a1cfc3d886
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-06-24 01:24:53 +0900

    refactor(component): update

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -3,61 +3,35 @@ import styles from "./style.module.scss";
 import { mergeClassName } from "../utils";
 // ______________________________________________________
 //
-type Size = "s" | "m" | "l";
-type SpecificProps = { size?: Size };
 type AriaLevel = 1 | 2 | 3 | 4 | 5 | 6;
 type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-type HeadingLikeTag = "div" | "p";
+type HeadingLikeTag = "p";
 type Tag = HeadingTag | HeadingLikeTag;
+type Element = HTMLHeadingElement | HTMLParagraphElement;
+// ______________________________________________________
+//
+type Size = "s" | "m" | "l";
+type SpecificProps = { size?: Size };
 // ______________________________________________________
 //
 type HeadingProps = { tag: HeadingTag; "aria-level"?: never };
 type HeadingLikeProps = { tag: HeadingLikeTag; "aria-level": AriaLevel };
-type TagProp = HeadingProps | HeadingLikeProps;
 type Props<T extends Tag> = T extends HeadingTag
-  ? HeadingProps & SpecificProps & React.ComponentPropsWithoutRef<T>
-  : HeadingLikeProps & SpecificProps & React.ComponentPropsWithoutRef<T>;
+  ? HeadingProps & SpecificProps & React.ComponentPropsWithRef<T>
+  : HeadingLikeProps & SpecificProps & React.ComponentPropsWithRef<T>;
 // ______________________________________________________
 //
-export const HeadingComponent = React.forwardRef<
-  HTMLHeadingElement,
-  Props<HeadingTag>
->(({ tag, className, size = "l", ...props }, ref) =>
-  React.createElement(tag, {
-    ref,
-    ...props,
-    className: mergeClassName(styles.heading, className),
-    "data-size": size,
-  })
-);
-
-export const HeadingLikeComponent = React.forwardRef<
-  HTMLParagraphElement | HTMLDivElement,
-  Props<HeadingLikeTag>
->(({ tag, className, size = "l", ...props }, ref) =>
-  React.createElement(tag, {
-    ref,
-    ...props,
-    className: mergeClassName(styles.heading, className),
-    role: "heading",
-    "data-size": size,
-    "aria-level": props["aria-level"],
-  })
-);
-// ______________________________________________________
-//
-export function Heading<T extends TagProp, K extends T["tag"]>(
-  props: Props<K>
-) {
-  switch (props.tag) {
-    case "h1":
-    case "h2":
-    case "h3":
-    case "h4":
-    case "h5":
-    case "h6":
-      return <HeadingComponent {...props} />;
-    default:
-      return <HeadingLikeComponent {...props} />;
-  }
-}
+export const Heading: <T extends Tag>(props: Props<T>) => JSX.Element =
+  React.forwardRef<Element, Props<Tag>>(
+    ({ tag, className, size, ...props }, ref) =>
+      React.createElement(tag, {
+        ref,
+        className: mergeClassName(styles.heading, className),
+        "data-size": size,
+        ...props,
+        ...(tag === "p" && {
+          role: "heading",
+          "aria-level": props["aria-level"],
+        }),
+      })
+  );
